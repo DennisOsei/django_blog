56e898c6465214f13c74d54e811ace3b97b46793
Dennis <dennisosetimss1@gmail.com>
2020-12-20 23:31:25 -0500

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "post_index"
